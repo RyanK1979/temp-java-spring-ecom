2b9af44b4788bf61417495265209f7531364283b
RyanK79 <Nermal5@msn.com>
2018-03-19 15:27:09 -0400

    going home

---
+++ src/main/java/org/wecancodeit/ecom/catalog/BrowseController.java
@@ -0,0 +1,8 @@
+package org.wecancodeit.ecom.catalog;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BrowseController {
+
+}
---
+++ src/main/java/org/wecancodeit/ecom/catalog/Product.java
@@ -0,0 +1,18 @@
+package org.wecancodeit.ecom.catalog;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Product {
+
+	@RequestMapping("/products")
+	public Collection<Product> getProducts() {
+		return Collections.singleton(new Product());
+
+	}
+
+}
