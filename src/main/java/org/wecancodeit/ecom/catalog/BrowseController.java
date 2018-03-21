d3203d178704eba69506980b4bb8e7eb3a9f55ab
RyanK79 <Nermal5@msn.com>
2018-03-21 11:42:27 -0400

    all the tests pass for now

--- src/main/java/org/wecancodeit/ecom/catalog/BrowseController.java
+++ src/main/java/org/wecancodeit/ecom/catalog/BrowseController.java
@@ -1,5 +1,7 @@
 package org.wecancodeit.ecom.catalog;
 
+import javax.annotation.Resource;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BrowseController {
 
+	@Resource
 	private CrudRepository<Product, Long> productRepo;
 
 	@RequestMapping("/products")
