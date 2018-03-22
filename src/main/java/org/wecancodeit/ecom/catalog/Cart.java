28d18c747f1416c50c948b19ba80c9352012378e
RyanK79 <Nermal5@msn.com>
2018-03-22 14:32:36 -0400

    updating so willie can download latest

---
+++ src/main/java/org/wecancodeit/ecom/catalog/Cart.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.ecom.catalog;
+
+import java.util.Collection;
+
+public class Cart {
+
+	public Collection<Product> products;
+
+	public Cart(Product... product) {
+
+	}
+
+}
---
+++ src/test/java/org/wecancodeit/ecom/catalog/cartTest.java
@@ -0,0 +1,30 @@
+package org.wecancodeit.ecom.catalog;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@JsonTest
+public class cartTest {
+
+	@Resource
+	private CrudRepository<Product, Long> productRepo;
+
+	@Resource
+	private CrudRepository<Cart, Long> cartRepo;
+
+	@Test
+	public void shouldAddItemToCart() {
+		Product shoes = productRepo.save(new Product("shoes"));
+		Cart cart = new Cart(shoes);
+		cartRepo.save(cart);
+		
+		assertThat(cart.getProducts)
+	}
+
+}
